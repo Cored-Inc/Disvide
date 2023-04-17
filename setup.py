aaa704ad87fe2b21eee680b39c51809f37128e04
FSCYT <arifisthebest567696@gmail.com>
2023-04-17 19:29:52 +0500

    fix(setup.py): Changed version to v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='disvide',
-    version='0.1',
+    version='0.2',
     license='MIT',
     author='Cored Developments',
     author_email='skyblockmohammed@gmail.com',
